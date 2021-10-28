ef29f58c755c011cebc26554b3698129565c812d
MilagrosLC <83854147+Milagros-Lc@users.noreply.github.com>
2021-10-28 16:47:29 -0500

    Agregando imagen (#5)
    * Imagen en el nav

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-// eslint-disable-next-line no-console
+
 console.log(example, data);
 
 
@@ -11,32 +11,43 @@ window.onload = function () {
     printNav();
     printChampions();
 
+
 }
 let arrayNav = ["Champions", "Ranking","imagen", "Download", "News"];
 
 function printNav() {
-    let temp, item, a, i;
+
+    let temp, item, a,b, i;
     temp = document.getElementById("idTemplate");
     //get the div element from the template:
     item = temp.querySelector("ul");
-    //for each item in the array:
+    //for each item in the array: 
     for (i = 0; i < arrayNav.length; i++) {
         //create elemnts li
         a = document.createElement("li");
-        //Add data from the array:
-
-        //(arrayNav[2]).innerHtml= <img src='./img/"+cards[rand][0]+"' />
+    
+       if(arrayNav[i] == arrayNav[2]){
+          
+       b=document.createElement("img");
+       b.src= "https://www.gamerfocus.co/wp-content/uploads/2013/12/league-of-legends-modo-showdown-riot-games-experimental-1.png";
+       console.log(b);
+       a = b;
+       
+    }else  
+     {
         a.textContent += arrayNav[i];
+    }
         //append the new node wherever you like:
         //document.temp2.appendChild(a);
         item.appendChild(a);
+   
+
+
     }
 }
 
-/* function printFoot(){
-
-} */
 
 function printChampions(){
   alert(data)
 }
+
