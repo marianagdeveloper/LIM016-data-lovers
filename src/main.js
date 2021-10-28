aa4a1cf2f6d9b6616618441cf2c0d56f64eb25e2
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-10-28 09:29:30 -0500

    Subiendo dependencias

--- src/main.js
+++ src/main.js
@@ -4,3 +4,14 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+
+window.onload= function (){
+    printNav();
+}
+
+function printNav() {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+  }
