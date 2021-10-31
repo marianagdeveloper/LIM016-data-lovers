36d92ad31bc63c2cf8d7d6193e54c36588302e0c
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-10-31 16:43:21 -0500

    Cards generados con los campeones completos

--- src/main.js
+++ src/main.js
@@ -2,29 +2,26 @@ import { example } from './data.js';
 import data from './data/lol/lol.js';
 
 const dataGet = JSON.stringify(data);
+
 const aux = JSON.parse(dataGet);
-let arrayData = Object.values(aux);
+const arrayData=(aux.data);
+console.log("imprime aca ",arrayData);
+let arraychampion= Object.values(arrayData)
+/* let arrayData = Object.values(aux);
+
+console.log("imprime en 4 elemen", arrayData); */
 let champion;
+
 window.onload = function () {
     printNav();
-  //  recorrerData();
-    printChampions(arrayData,"Aatrox");
-    printChampions(arrayData,"Alistar");
-    printChampions(arrayData,"Vladimir");
-    printChampions(arrayData,"Akali"); 
-    printChampions(arrayData,"Alistar");
-    printChampions(arrayData,"Vladimir");
-    printChampions(arrayData,"Akali"); 
-    printChampions(arrayData,"Aatrox");
-    printChampions(arrayData,"Akali"); 
-    printChampions(arrayData,"Alistar");
+    //  recorrerData();
+    recorrerData();  
 }
-
-/* function recorrerData(){
- 
-} */
-
-
+function recorrerData(){ 
+    for(let i=0;i<arraychampion.length;i++){
+        printChampions(arraychampion[i]); 
+    }
+}
 /* ---Create array Nav dinamic------ */
 let arrayNav = ["Champions", "Ranking", "https://www.gamerfocus.co/wp-content/uploads/2013/12/league-of-legends-modo-showdown-riot-games-experimental-1.png", "Download", "News"];
 /* ----Create function Nav dinamic --- */
@@ -53,41 +50,39 @@ function printNav() {
         item.appendChild(list);
     }
 }
-
 ///---------End of function Nav -------
 /* ------------ Get Id div Cards ----- */
 
 let divChampion = document.getElementById("divShowChampions");
 
-
 /* --Create function PrintChampions in cards-- */
-function printChampions(arrayData,champion) {
-    let imagen, varChampion;
-    console.log("imprime",arrayData) 
-    varChampion = arrayData[3][champion];
+function printChampions(arraychampion) {
+    let imagen, varChampion;  
+    
+   /*  console.log("imprime aca funcion ", arraychampion);
+    console.log("imprime aca ", arraychampion); */
+    varChampion =arraychampion;
     imagen = varChampion.splash;
     let cards = document.createElement("div");
     divChampion.appendChild(cards);
-    cards.setAttribute("class","card");
+    cards.setAttribute("class", "card");
     let imgbox = document.createElement("div");
     cards.appendChild(imgbox);
-    imgbox.setAttribute("class","img-box");
+    imgbox.setAttribute("class", "img-box");
     let x = document.createElement("img");
     imgbox.appendChild(x);
-    x.setAttribute("class","imagen");
+    x.setAttribute("class", "imagen");
     x.src = imagen;
     let conten = document.createElement("div");
     cards.appendChild(conten);
-    conten.setAttribute("class","content");
+    conten.setAttribute("class", "content");
     let y = document.createElement("h2");
     conten.appendChild(y);
-    console.log("nombre "+varChampion.id);
-    let text=document.createTextNode(varChampion.id);
+    let text = document.createTextNode(varChampion.id);
     y.appendChild(text);
     let p = document.createElement("p");
     conten.appendChild(p);
-    console.log("texto "+varChampion.id);
-    let texto=document.createTextNode(varChampion.id);
+    let texto = document.createTextNode(varChampion.blurb);
     p.appendChild(texto);
 }
 ///// esto es una prueba despues de comentar el map y ahora enviare parametros
