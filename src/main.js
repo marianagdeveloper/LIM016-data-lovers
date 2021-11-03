9dcb44b8d709fb223bf3bd12ab236fac7b846adc
MilagrosLC <83854147+Milagros-Lc@users.noreply.github.com>
2021-11-02 23:01:00 -0500

    Filtro por rol V.0.0.8 (#8)
    * Creando Nav manipulando el DOM
    
    * v1
    
    * Imagen en el nav
    
    * Nuevo js
    
    * Nav responsive - mostrar imagen de campeon
    
    * Card añadido mostrando campeon y su nombre
    
    * v.1.0
    
    * Section champions order
    
    * Cards generados con los campeones completos
    
    * Funcion implementada en data.js
    
    * Nombre de la funcion cambiada y nuevos esilos para rol
    
    * Estilos para roles y scroll añadido
    
    * Creando funcion para generar botones de rol dinamico
    
    * v.0.0.8 - Filtro por rol

--- src/main.js
+++ src/main.js
@@ -1,29 +1,35 @@
-import { example } from './data.js';
-import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-
-console.log(example, data);
-
+import { dataChampions } from './data.js';
+// listo funcion en data hecha
+let arraychampion = dataChampions();
+let divChampion = document.getElementById("divShowChampions");
+let arrayButton = ["Fighter", "Marksman", "Mage", "Assassin", "Tank", "Support"];
+let button, btnRol = [], textButton, cards, rolChampion;
 
 window.onload = function () {
     printNav();
-    printChampions();
-
+    recorrerData();
+    printButton();
 
 }
-let arrayNav = ["Champions", "Ranking","imagen", "Download", "News"];
-
+//probando foreach listoo o
+function recorrerData() {
+    arraychampion.forEach(element => printChampions(element));
+}
+/* ---Create array Nav dinamic------ */
+let arrayNav = ["Champions", "Ranking", "https://www.gamerfocus.co/wp-content/uploads/2013/12/league-of-legends-modo-showdown-riot-games-experimental-1.png", "Download", "News"];
+/* ----Create function Nav dinamic --- */
 function printNav() {
 
-    let temp, item, a,b, i;
-    temp = document.getElementById("idTemplate");
+    let temp, item, list, b, i, enlace;
+    temp = document.getElementById("navDinamic");
+
     //get the div element from the template:
     item = temp.querySelector("ul");
     //for each item in the array: 
     for (i = 0; i < arrayNav.length; i++) {
         //create elemnts li
+
         a = document.createElement("li");
     
        if(arrayNav[i] == arrayNav[2]){
@@ -42,12 +48,92 @@ function printNav() {
         item.appendChild(a);
    
 
-
     }
 }
+///---------End of function Nav -------
 
 
-function printChampions(){
-  alert(data)
+/* --Create function PrintChampions in cards-- */
+function printChampions(arraychampion) {
+    console.log(arraychampion);
+    let imagen;
+    imagen = arraychampion.splash;
+    cards = document.createElement("div");
+    divChampion.appendChild(cards);
+    cards.setAttribute("class", "card");
+    let imgbox = document.createElement("div");
+    cards.appendChild(imgbox);
+    imgbox.setAttribute("class", "img-box");
+    let x = document.createElement("img");
+    imgbox.appendChild(x);
+    x.setAttribute("class", "imagen");
+    x.src = imagen;
+    let conten = document.createElement("div");
+    cards.appendChild(conten);
+    conten.setAttribute("class", "content");
+    let y = document.createElement("h2");
+    conten.appendChild(y);
+    let text = document.createTextNode(arraychampion.id);
+    y.appendChild(text);
+    let p = document.createElement("p");
+    conten.appendChild(p);
+    let texto = document.createTextNode(arraychampion.blurb);
+    p.appendChild(texto);
 }
+
+//// acaaa estoy empezando de nuevooo
+
+
+/* ----Create function Button rol dinamic --- */
+function printButton() {
+    let i;
+
+    rolChampion = document.getElementById("rolChampion");
+
+    for (i = 0; i < arrayButton.length; i++) {
+        button = document.createElement("button");
+        textButton = document.createTextNode(arrayButton[i])
+        button.appendChild(textButton);
+        rolChampion.appendChild(button);
+        button.setAttribute("class", "btn third");
+        button.setAttribute('id', (arrayButton[i]));
+
+        btnRol[i] = document.getElementById(arrayButton[i]);
+    
+     
+}
+    for(let i=0;i<btnRol.length;i++){
+        btnRol[i].addEventListener('click',function() {
+            filter(btnRol[i]);
+            
+        });
+    }
+ 
+}
+/* ----Create function filter eventlistener --- */
+function filter(btnRol) {
+    
+    divChampion.innerHTML = "";
+    arraychampion.forEach(element => filterRol(element,btnRol));
+
+
+}
+
+/* ----Create function filterRol call filter --- */
+function filterRol(arraychampion,btnRol) {
+ 
+            for (let i = 0; i < (arraychampion.tags).length; i++) {
+      
+            if(arraychampion.tags[i] == btnRol.id) {
+
+                printChampions(arraychampion);
+    
+            } 
+            
+        }
+    }
+   ///khbefhiweb kjnfnwekjnfwe  kmnkmn
+///---------End of function Print Button -------<<
+
+
 
