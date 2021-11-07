a63ea2a3564884eb1f946e9e442b59d347424570
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-11-06 21:00:04 -0500

    modal implementado 6-11

--- src/main.js
+++ src/main.js
@@ -2,22 +2,99 @@
 import { dataChampions } from './data.js';
 // listo funcion en data hecha
 let arraychampion = dataChampions();
-
+console.log(arraychampion);
 let divChampion = document.getElementById("divShowChampions");
 let inputSearch = document.getElementById("inputSearch");
 let btnSearch = document.getElementById("btnSearch");
-let key, textoKey=[],ki=0;
+
 let arrayButton = ["All", "Fighter", "Marksman", "Mage", "Assassin", "Tank", "Support"];
-let button, btnRol = [], textButton, cards=[], rolChampion;
+let button, btnRol = [], textButton, cards = [], rolChampion;
 let modalChampion;
+//modal
+let cerrar = document.getElementById("close");
+let modal = document.getElementById("modal");
+let modalC = document.getElementById("modal-container");
 
 window.onload = function () {
     printNav();
-    printChampions(arraychampion);
+    recorrerData();
     printButton();
 
 }
+//modal
+function printModal(champions) {
+   
+    const containerModal = document.createElement("div");
+    containerModal.setAttribute("class","containerModal")
+    modal.appendChild(containerModal);
 
+    const divNameRole= document.createElement("div");
+    divNameRole.setAttribute("class","divNameRole");
+    containerModal.appendChild(divNameRole);
+    const nameModal = document.createElement("h2");
+    const textName = document.createTextNode(champions.name.toUpperCase());
+    nameModal.setAttribute("class","nameModal")
+    nameModal.appendChild(textName);
+    divNameRole.appendChild(nameModal);
+    const containerRole = document.createElement("div");
+    containerRole.setAttribute("class","containerRole");
+    const textTittleRole= document.createTextNode("Rol:");
+    containerRole.appendChild(textTittleRole);
+    divNameRole.appendChild(containerRole);
+    const role = document.createElement("p");
+    role.setAttribute("class","role");
+    const textRole = document.createTextNode(champions.tags);
+    console.log(role);
+    role.appendChild(textRole);
+    containerRole.appendChild(role);
+    const containerImageDescrip = document.createElement("div");
+    containerImageDescrip.setAttribute("class","containerImageDescrip");
+    containerModal.appendChild(containerImageDescrip);
+    const containerImage = document.createElement("div");
+    containerImage.setAttribute("class","containerImage");
+    containerImageDescrip.appendChild(containerImage);
+    const imageModal = document.createElement("img");
+    imageModal.setAttribute("class","imageModal")
+    imageModal.src = champions.splash;
+    containerImage.appendChild(imageModal);
+    const descriptionModal = document.createElement("p");
+    const textDescription = document.createTextNode(champions.blurb);
+    descriptionModal.setAttribute("class","descriptionModal")
+    descriptionModal.appendChild(textDescription);
+    containerImageDescrip.appendChild(descriptionModal);
+   const containerAbilities=document.createElement("div");
+   containerAbilities.setAttribute("class","containerAbilities")
+   containerModal.appendChild(containerAbilities);
+   const divAttack = document.createElement("div");
+   divAttack.setAttribute("class","divAttack")
+   containerAbilities.appendChild(divAttack);
+   const divDefense = document.createElement("div");
+   divDefense.setAttribute("class","divDefense")
+   containerAbilities.appendChild(divDefense);
+   const divMagic = document.createElement("div");
+   divMagic.setAttribute("class","divMagic")
+   containerAbilities.appendChild(divMagic);
+   const divDifficulty = document.createElement("div");
+   divDifficulty.setAttribute("class","divDifficulty")
+   containerAbilities.appendChild(divDifficulty);
+
+ /*   attack: 8,
+   defense: 4,
+   magic: 3,
+   difficulty: 4 */
+   
+
+    //properties modal
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+
+
+    cerrar.addEventListener("click", function (e) {
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+        containerModal.innerHTML = "";
+    });
+}
 /* ---Create array Nav dinamic------ */
 let arrayNav = ["Champions", "Ranking", "https://www.gamerfocus.co/wp-content/uploads/2013/12/league-of-legends-modo-showdown-riot-games-experimental-1.png", "Download", "News"];
 /* ----Create function Nav dinamic --- */
@@ -50,46 +127,57 @@ function printNav() {
     }
 }
 ///---------End of function Nav -------
-let idchampion=[];
+
 /* --Create function PrintChampions in cards-- */
+function recorrerData() {
+    arraychampion.forEach(element => printChampions(element));
+
+}
 function printChampions(arraychampion) {
-    for (let i = 0; i < arraychampion.length; i++) {     
+
     let imagen;
-    imagen = arraychampion[i].splash;
-    cards[i] = document.createElement("div");
-    divChampion.appendChild(cards[i]);
-    cards[i].setAttribute("class", "card");
+    imagen = arraychampion.splash;
+    cards = document.createElement("div");
+    divChampion.appendChild(cards);
+    cards.setAttribute("class", "card");
+    cards.setAttribute('id', (arraychampion.key));
     let imgbox = document.createElement("div");
-    cards[i].appendChild(imgbox);
+    cards.appendChild(imgbox);
     imgbox.setAttribute("class", "img-box");
     let x = document.createElement("img");
     imgbox.appendChild(x);
     x.setAttribute("class", "imagen");
     x.src = imagen;
     let conten = document.createElement("div");
-    cards[i].appendChild(conten);
+    cards.appendChild(conten);
     conten.setAttribute("class", "content");
     let y = document.createElement("h2");
     conten.appendChild(y);
-    let text = document.createTextNode(arraychampion[i].id);
+    let text = document.createTextNode(arraychampion.id);
     y.appendChild(text);
     let p = document.createElement("p");
     conten.appendChild(p);
-    let texto = document.createTextNode(arraychampion[i].blurb);
+    let texto = document.createTextNode(arraychampion.blurb);
     p.appendChild(texto);
-  
-    cards[i].setAttribute('id', (arraychampion[i].key));
-   idchampion[i] = document.getElementById(arraychampion[i].key);
-    }
-    for (let i = 0; i < idchampion.length; i++) {
-        idchampion[i].addEventListener('click', function () {
-           vistaDetalle( idchampion[i]);
-        });
-    }
+
+
+    let idcard = document.getElementById(arraychampion.key);
+
+    idcard.addEventListener("click", function (e) {
+        console.log("arraychampion.key:", arraychampion.key);
+        // console.log("evento:", e.path[2].attributes[1]);
+        printModal(arraychampion);
+    });
+
 }
+// let abrir = document.querySelectorAll(".card");
+
 
 //// acaaa estoy empezando de nuevooo
 
+
+////// hol ahooa knk knkn nknkc dsmm
+///Egg krmfkwemf
 
 /* ----Create function Button rol dinamic --- */
 function printButton() {
@@ -112,139 +200,36 @@ function printButton() {
     }
     for (let i = 0; i < btnRol.length; i++) {
         btnRol[i].addEventListener('click', function () {
-            filter(btnRol[i]);
+            filterByRole(btnRol[i]);
 
         });
     }
 
 }
 /* ----Create function filter eventlistener --- */
-function filter(btnRol) {
 
+function filterByRole(btnRol) {
     divChampion.innerHTML = "";
     if (btnRol.id == "All") {
         recorrerData();
     } else
-        arraychampion.forEach(element => filterRol(element, btnRol));
-
-
+        arraychampion.filter(e => e.tags.includes(btnRol.id)).map(e => printChampions(e));
 
 }
 
-/* ----Create function filterRol call filter --- */
-function filterRol(arraychampion, btnRol) {
-
-    for (let i = 0; i < (arraychampion.tags).length; i++) {
-
-        if (arraychampion.tags[i] == btnRol.id) {
-
-            printChampions(arraychampion);
-
-        }
-
-
-    }
-}
-
-///---------End of function Print Button -------<<
-//haciendo 
-
+///---------Search -------<<
 inputSearch.addEventListener("keyup", function () {
     divChampion.innerHTML = '';
     let texto = inputSearch.value.toLowerCase();
-    arraychampion.forEach(element => filtrar(element, texto));
+    const searchHero = arraychampion.filter(e => e.name.toLowerCase().indexOf(texto) > -1).map(e => printChampions(e));
 
-
-}
-);
-
-function filtrar(element, texto) {
-
-    let nombre = (element.name).toLowerCase();
-
-    if ((nombre.indexOf(texto)) > -1) {
-
-        printChampions(element);
-
-
+    if (searchHero.length == 0) {
+        divChampion.innerHTML = '<p>Champion no found</p>';
     }
-    /*    if (divChampion.innerHTML == '' ) {
-           divChampion.innerHTML = "Champions no found";
-         
-          
-       } */
-}
 
-/*---------  blurb: "A healer gifted with the magic of the stars, Soraka holds all living creatures close to her heart. She was once a celestial being, but she sacrificed her immortality and entered the world of mortals. So long as evil threatens life in Valoran, Soraka ..."
- id: "Soraka"
- image: {full: 'Soraka.png', sprite: 'champion3.png', group: 'champion', x: 336, y: 0, …}
- img: "https://www.masterypoints.com/assets/img/lol/champion_icons/Soraka.png"
- info: {attack: 2, defense: 5, magic: 7, difficulty: 3}
- key: "16"
- name: "Soraka"
- partype: "MP"
- -----------splash: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Soraka_0.jpg"
- stats: {hp: 529.04, hpperlevel: 78, mp: 350.8, mpperlevel: 60, movespeed: 325, …}
- tags: (2) ['Support', 'Mage']
- title: "the Starchild"
- version: "6.24.1" */
+});
+//aki ki njnd jjdnd jnun jnu aaaa aa
 
 
-function vistaDetalle(idcard) {
-
-    let modal = document.getElementById("tvesModal");
-    let span = document.getElementsByClassName("close")[0];
-
-    idcard.addEventListener('click', function () {
-       
-        arraychampion.forEach(element => printChampionsVista(element, modal,idcard));
-
-        modal.style.display = "block";
-
-        document.body.style.position = "static";
-        document.body.style.height = "100%";
-        document.body.style.overflow = "hidden";
-    });
-
-    span.addEventListener('click', function () {
-    
-        modal.style.display = "none";
-        modalChampion.innerHTML="";
-        document.body.style.position = "inherit";
-        document.body.style.height = "auto";
-        document.body.style.overflow = "visible";
-    });
-}
 
 
-function printChampionsVista(arraychampion, modal,idcard) {
-
-    if (idcard.id == arraychampion.key) {
-       
-        let imagen;
-        imagen = arraychampion.splash;
-        modalChampion = document.createElement("div");
-        modal.appendChild(modalChampion);
-        modalChampion.setAttribute("class", "modalChampion");
-        let contImagen = document.createElement("div");
-        modalChampion.appendChild(contImagen);
-        /* imgbox.setAttribute("class", "img-box"); */
-        let img = document.createElement("img");
-        contImagen.appendChild(img);
-        img.setAttribute("class", "imgmodal");
-        img.src = imagen;
-
-        let contenModal = document.createElement("div");
-        modalChampion.appendChild(contenModal);
-        /*  contenModal.setAttribute("class", "content"); */
-        let h2name = document.createElement("h2");
-        contenModal.appendChild(h2name);
-        let textName = document.createTextNode(arraychampion.name);
-        h2name.appendChild(textName);
-        let descrip = document.createElement("p");
-        contenModal.appendChild(descrip);
-        let textoDescrip = document.createTextNode(arraychampion.blurb);
-        descrip.appendChild(textoDescrip);  
-    }
-}
-///hasta aqui estamos bien no la malogres aaa
