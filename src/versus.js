85ef106d2bf2708963cea9433525af1f376e1962
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-11-15 23:45:04 -0500

    tengo sueño

--- src/versus.js
+++ src/versus.js
@@ -1,14 +1,14 @@
 
 import { dataChampions } from './data.js';
- let image = [];
+let image = [];
 let arraychampion = [];
 let arrayRole = ["Fighter", "Marksman", "Mage", "Assassin", "Tank", "Support"];
 let arrayIdPanel = ["uno", "dos", "tres", "cuatro", "cinco", "seis"];
-let containerImage=document.getElementById("img1");
-let containerImage2=document.getElementById("img2");
-let pName1=document.getElementById("pName1");
-let pName2=document.getElementById("pName2");
-
+let containerImage = document.getElementById("img1");
+let containerImage2 = document.getElementById("img2");
+let pName1 = document.getElementById("pName1");
+let pName2 = document.getElementById("pName2");
+let divGraficVersus = document.getElementById("graficVersus");
 async function carga() {
   await dataChampions().then((data) => {
     arraychampion = data;
@@ -24,53 +24,137 @@ async function carga() {
     cinco.addEventListener("click", championIcon(arrayRole[4], arrayIdPanel[4]));
     seis.addEventListener("click", championIcon(arrayRole[5], seis));
 
-    
 
-   
-    
+
+
+
 
 
   });
 }
 carga();
- 
-window.onload = ()=>{
- 
+
+window.onload = () => {
+
   carga2(arraychampion);
-} 
+}
 
 function carga2(arraychampion) {
 
   let ivi = [];
-  let name=[];
+  let name = [];
   for (let i = 0; i < arraychampion.length; i++) {
 
-   image[i]=arraychampion[i].splash;
+    image[i] = arraychampion[i].splash;
 
     ivi[i] = document.getElementById(arraychampion[i].id);
- name[i]=arraychampion[i].name;
+    name[i] = arraychampion[i].name;
     ivi[i].addEventListener("click", function () {
-      console.log("aaaa",ivi[i])
-console.log("imagen ruta "+image[i]);
-     
-if( containerImage.src==""){
+      console.log("aaaa", ivi[i])
+      console.log("imagen ruta " + image[i]);
 
-  console.log("acaaaaaa");
-  containerImage.src = image[i];
-  let texto=document.createTextNode( name[i]);
-  pName1.appendChild(texto);
-}else{  
-  containerImage2.src=image[i];
-  let texto2=document.createTextNode( name[i]);
-  pName2.appendChild(texto2);
-  
-}
+      if (containerImage.src == "") {
+
+        console.log("acaaaaaa");
+        containerImage.src = image[i];
+        let texto = document.createTextNode(name[i]);
+        let primero = name[i];
+        console.log("eeeeeeeeeeeeeeeeeeee", primero);
+        pName1.appendChild(texto);
+      } else {
+        containerImage2.src = image[i];
+        let texto2 = document.createTextNode(name[i]);
+        let segundo = name[i];
+        console.log("eeeeeeeeeeeeeeeeeeee", segundo);
+        pName2.appendChild(texto2);
+        championElegido(primero, segundo);
+      }
+
+
+
+      //aqui realizar los graficos
+
+      function championElegido(primero, segundo) {
+
+        for (let i = 0; i < arraychampion.length; i++) {
+
+          if (arraychampion[i].name == primero) {
+            let cpp = arraychampion[i];
+            for (let i = 0; i < arraychampion.length; i++) {
+              if (arraychampion[i].name == segundo) {
+                let css = arraychampion[i];
+              }
+            }
+            /* eslint-disable */
+            google.charts.setOnLoadCallback(drawChartVersus);
+
+            function drawChartVersus() {
+
+              let arrayColors = ["blue", "red"];
+              let datas = google.visualization.arrayToDataTable([
+//falta cambiar 
+                ['Abilities', cpp.name, css.name],
+                ['Hp', cpp.stats.hp, arrayColors[0]],
+                ['Move Speed', cpp.stats.movespeed, arrayColors[0]],
+                ['hpperlevel', cpp.stats.hpperlevel, arrayColors[0]],
+                ['spellblock', cpp.stats.spellblock, arrayColors[0]],
+                ['armor', cpp.stats.armor, arrayColors[0]],
+                ['hpregen', cpp.stats.hpregen, arrayColors[0]],
+                ['Atacck Range', cpp.stats.attackrange, arrayColors[0]],
+                ['Crit', cpp.stats.crit, arrayColors[0]],
+                ['Attack Damage', cpp.stats.attackdamage, arrayColors[0]],
+                ['attackdamageperlevel', cpp.stats.attackdamageperlevel, arrayColors[0]],
+
+                //fdsfsdfd
+                /* var data = google.visualization.arrayToDataTable([
+                  ['Galaxy', 'Distance', 'Brightness'],
+                  ['Canis Major Dwarf', 8000, 23.3],
+                  ['Sagittarius Dwarf', 24000, 4.5],
+                  ['Ursa Major II Dwarf', 30000, 14.3],
+                  ['Lg. Magellanic Cloud', 50000, 0.9],
+                  ['Bootes I', 60000, 13.1]
+                ]); */
+                //dsfsdfdsf
+
+              ]);
+
+              let view = new google.visualization.DataView(datas);
+              view.setColumns([0, 1,
+                {
+                  calc: "stringify",
+                  sourceColumn: 1,
+                  type: "string",
+                  role: "annotation"
+                },
+                2]);
+
+              let options = {
+                title: "The most populares Champions",
+                width: 600,
+                height: 350,
+                bar: { groupWidth: "95%" },
+                legend: { position: "none" },
+              };
+              let chart = new google.visualization.ColumnChart(divGraficVersus);
+              chart.draw(view, options);
+
+            }
+          }
+        }
+      }
+
+
+
+
+      //aqui termina grafico
 
     });
 
   }
 
-} 
+}
+
+
 
 function championIcon(btnRol, m) {
   let array = arraychampion.filter(e => e.tags.includes(btnRol));
@@ -93,7 +177,7 @@ function iconTemplate(champion) {
     </div>
   `
 
-  
+
 }
 
 const tabLinks = document.querySelectorAll(".tabs a");
